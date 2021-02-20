2d5082222de48a6ff304d7e65e6a206b723403a0
Muhammad-Jibril <jbashir52@gmail.com>
2021-02-21 03:07:47 +0800

    remove unneeded imports, add FetchPrice CurrencyId

--- src/lib.rs
+++ src/lib.rs
@@ -27,18 +27,14 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use sp_std::prelude::*;
-
 use adapters::{BoundedPriorityQueue, BoundedDeque};
 use codec::{Decode, Encode};
 use core::cmp::{max, min, Ord, Ordering};
 use fixed::{types::extra::U64, FixedU128};
 use frame_support::pallet_prelude::*;
 
-use stp258::*;
-use serp_market::*;
-
+use serml_traits::*;
 use num_rational::Ratio;
-use orml_traits::BasicCurrency;
 use sp_runtime::{
 	traits::{CheckedMul, Zero},
 	PerThing, Perbill, RuntimeDebug,
@@ -53,9 +49,9 @@ mod mock;
 mod tests;
 
 /// Expected price oracle interface. `fetch_price` must return the amount of SettCurrency exchanged for the tracked value.
-pub trait FetchPrice<Balance> {
+pub trait FetchPrice<CurrencyId> {
 	/// Fetch the current price.
-	fn fetch_price() -> Balance;
+	fn fetch_price() -> CurrencyId;
 }
 
 /// The pallet's configuration trait.
